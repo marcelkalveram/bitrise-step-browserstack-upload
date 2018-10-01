bd1c498f074932aaed0af1c7187c22893f74df7e
Marcel Kalveram <marcel.kalveram@googlemail.com>
2018-10-01 17:50:34 +0200

    first commit

---
+++ step.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+echo "we need the app's APK path: $apk_path"
+echo "we need a BrowserStack username: $browserstack_username"
+echo "we need a BrowserStack password: $browserstack_password"
+
+RESPONSE="$(curl -u $browserstack_username:$browserstack_password -X POST https://api-cloud.browserstack.com/app-automate/upload -F file=@$apk_path)"
+echo "response is: $RESPONSE"
+
+echo "$RESPONSE" | jq '.app_url' | envman add --key BROWSERSTACK_APP_URL
