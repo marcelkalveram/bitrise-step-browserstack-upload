caa8c0fba5c1a0fc03c5998f207940249f9f5899
Marcel Kalveram <marcel.kalveram@googlemail.com>
2018-10-05 17:47:44 +0200

    update variable names and allow for IPA path

--- step.sh
+++ step.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 
-echo "we need the app's APK path: $apk_path"
+echo "we need the app's APK path: $upload_path"
 echo "we need a BrowserStack username: $browserstack_username"
-echo "we need a BrowserStack password: $browserstack_password"
+echo "we need a BrowserStack password: $browserstack_access_key"
 
-RESPONSE="$(curl -u $browserstack_username:$browserstack_password -X POST https://api-cloud.browserstack.com/app-automate/upload -F file=@$apk_path)"
+RESPONSE="$(curl -u $browserstack_username:$browserstack_access_key -X POST https://api-cloud.browserstack.com/app-automate/upload -F file=@$upload_path)"
 echo "response is: $RESPONSE"
 
 echo "$RESPONSE" | jq -j '.app_url' | envman add --key BROWSERSTACK_APP_URL
