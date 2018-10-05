e9cd16f2b4558f9285a2ec6ba8edd5d6f452f68b
Marcel Kalveram <marcel.kalveram@googlemail.com>
2018-10-05 19:19:11 +0200

    remove comments from shell script

--- step.sh
+++ step.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-set -ex
-
-echo "we need the app's APK path: $upload_path"
-echo "we need a BrowserStack username: $browserstack_username"
-echo "we need a BrowserStack password: $browserstack_access_key"
-
 RESPONSE="$(curl -u $browserstack_username:$browserstack_access_key -X POST https://api-cloud.browserstack.com/app-automate/upload -F file=@$upload_path)"
-echo "response is: $RESPONSE"
-
 echo "$RESPONSE" | jq -j '.app_url' | envman add --key BROWSERSTACK_APP_URL
