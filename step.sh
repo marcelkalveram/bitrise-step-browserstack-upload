83fbd17e0608f2e587866c740d35cdee2619cc9b
Shreyansh Pandey <shreyanshvalentino@gmail.com>
2019-08-06 17:22:02 +0900

    Check if speed limit goes down, then close

--- step.sh
+++ step.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-RESPONSE="$(curl -u $browserstack_username:$browserstack_access_key -X POST https://api-cloud.browserstack.com/app-automate/upload -F file=@$upload_path)"
+RESPONSE="$(curl -u $browserstack_username:$browserstack_access_key -X POST https://api-cloud.browserstack.com/app-automate/upload -F file=@$upload_path --speed-time 10 --speed-limit 1000)"
 echo "$RESPONSE" | jq -j '.app_url' | envman add --key BROWSERSTACK_APP_URL
