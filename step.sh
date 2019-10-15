94d52f92bbc606bf1d7febab415252e1b6a729a2
Sinan Gunes <sinan@medici.md>
2019-10-15 09:10:04 +0300

    Allowing space in file names

--- step.sh
+++ step.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 set -x
-RESPONSE="$(curl -u $browserstack_username:$browserstack_access_key -X POST https://api-cloud.browserstack.com/app-automate/upload -F file=@$upload_path -F 'data={"custom_id": "'$custom_id'"}')"
-echo "$RESPONSE" | jq -j '.app_url' | envman add --key BROWSERSTACK_APP_URL
+curl -u "$browserstack_username:$browserstack_access_key" -X POST https://api-cloud.browserstack.com/app-automate/upload -F "file=@$upload_path" -F 'data={"custom_id": "'$custom_id'"}' | jq -j '.app_url' | envman add --key BROWSERSTACK_APP_URL
